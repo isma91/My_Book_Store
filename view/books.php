b88fe8915f5c0f650a1fe918dc444bc2f964b2c1
isma91 <ismaydogmus@gmail.com>
2016-06-06 16:37:19 +0200

    adding the books view page

---
+++ view/books.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="My_Mini_Tweet is a good way to test your own ego !!" />
+    <title>Welcome to My_Mini_Tweet</title>
+    <?php include "media.html" ?>
+    <script src="media/js/books.js"></script>
+</head>
+<body>
+    <div class="container" id="the_body">
+    <?php var_dump($_SESSION); ?>
+    </body>
+    </html>
