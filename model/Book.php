89d9a666e29e60664747a40695471162b9bc8ea8
isma91 <ismaydogmus@gmail.com>
2016-06-06 16:43:49 +0200

    adding the books modal

---
+++ model/Book.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Book.php
+*
+* The model of Book
+*
+* PHP 7.0.4-7ubuntu2.1 (cli) ( NTS )
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+namespace model;
+class Book
+{
+}
