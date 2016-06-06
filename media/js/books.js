da4abd5b7e8f8aca895557bd021b484267034f06
isma91 <ismaydogmus@gmail.com>
2016-06-06 16:37:03 +0200

    adding the books page js file

---
+++ media/js/books.js
@@ -0,0 +1,17 @@
+/*jslint browser: true, node : true*/
+/*jslint devel : true*/
+/*global $, document, this*/
+$(document).ready(function(){
+    var path_to_ajax = "public_api/index.php";
+    function press_enter (selector, go_function) {
+        $(document).on('keyup', selector, function(event) {
+            if (event.keyCode === 13) {
+                go_function();
+            }
+        });
+    }
+    $.post(path_to_ajax, {action: 'get_all_books'}, function(data) {
+        data = JSON.parse(data);
+        console.log(data);
+    });
+});
