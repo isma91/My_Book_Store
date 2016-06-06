4e0c8972777f2fc3ef10279c23c9d06a5bbe1b72
isma91 <ismaydogmus@gmail.com>
2016-06-06 13:01:31 +0200

    adding the simple file of the config

---
+++ config-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Config-sample.php
+*
+* This is the config file, you need to change the host, dbname, user, password
+* to use this project and make it work !!
+* Don't forget to rename this file to config.php !!
+*
+* PHP 7.0.4-7ubuntu2.1 (cli) ( NTS )
+*
+* @category Model
+* @package  Model
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+return [
+	'host' => 'localhost',
+	'dbname' => 'my_mini_tweet',
+	'user' => 'root',
+	'password' => '******'
+];
