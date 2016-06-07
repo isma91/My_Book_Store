1b31fc9d7344d427a97148eb3347606043ff2651
isma91 <ismaydogmus@gmail.com>
2016-06-07 13:41:34 +0200

    adding empty view file

---
+++ media/js/add_customer.js
@@ -0,0 +1,13 @@
+/*jslint browser: true, node : true*/
+/*jslint devel : true*/
+/*global $, document, this*/
+$(document).ready(function(){
+    var path_to_ajax = "public_api/index.php";
+    function press_enter (selector, go_function) {
+        $(document).on('keyup', selector, function(event) {
+            if (event.keyCode === 13) {
+                go_function();
+            }
+        });
+    }
+});
