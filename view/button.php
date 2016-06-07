71d95c19af5cb109f85a4eb99dd27376b9f71b17
isma91 <ismaydogmus@gmail.com>
2016-06-07 14:44:31 +0200

    the 4 button in all view are in another file

---
+++ view/button.php
@@ -0,0 +1,6 @@
+<div class="row end_button">
+	<button class="waves-effect btn-flat" id="logout" token='<?php echo $_SESSION["token"] ?>'>Logout</button>
+	<a class="waves-effect btn-flat" href="?page=add_book">Add Book</a>
+	<a class="waves-effect btn-flat" href="?page=add_customer">Add Customer</a>
+	<a class="waves-effect btn-flat" href="?page=add_command">Add Command</a>
+</div>
