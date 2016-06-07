5d3aa5fad0b7871e9f8d379b6a15c15c7e5e79f7
isma91 <ismaydogmus@gmail.com>
2016-06-07 13:39:41 +0200

    fix infinite loop

--- index.php
+++ index.php
@@ -17,15 +17,15 @@ require 'autoload.php';
 use controller\UsersController;
 $connected = UsersController::is_connected();
 function go_to_view ($page) {
-	if ($page === "home_page.php") {
-		if (UsersController::is_connected()) {
-			go_to_view("books.php");
+	if ($page === "home_page") {
+		if (UsersController::is_connected() === true) {
+			include "./view/books.php";
 		} else {
-			go_to_view("home_page.php");
+			include "./view/home_page.php";
 		}
 	} else {
-		if (UsersController::is_connected()) {
-			include "./view/" . $page;
+		if (UsersController::is_connected() === true) {
+			include "./view/" . $page . ".php";
 		} else {
 			include "./view/home_page.php";
 		}
@@ -34,15 +34,24 @@ function go_to_view ($page) {
 if ($_GET) {
 	switch ($_GET["page"]) {
 		case 'home':
-		go_to_view("home_page.php");
+		go_to_view("home_page");
 		break;
 		case 'books':
-		go_to_view("books.php");
+		go_to_view("books");
+		break;
+		case 'add_book':
+		go_to_view("add_book");
+		break;
+		case 'add_customer':
+		go_to_view("add_customer");
+		break;
+		case 'add_command':
+		go_to_view("add_command");
 		break;
 		default:
-		go_to_view("books.php");
+		go_to_view("books");
 		break;
 	}
 } else {
-	go_to_view("home_page.php");
+	go_to_view("home_page");
 }
