74487df404886e42d5771cf43e1d9a72a5ac76e7
isma91 <ismaydogmus@gmail.com>
2016-06-07 13:17:01 +0200

    adding more div

--- view/books.php
+++ view/books.php
@@ -9,6 +9,32 @@
 </head>
 <body>
     <div class="container" id="the_body">
-    <?php var_dump($_SESSION); ?>
-    </body>
-    </html>
+        <div class="row mui-panel" id="the_menu">
+            <h1 class="title">Welcome to My_Book_Store !!</h1>
+            <h2 class="title">Here is the list of all books !!</h2>
+            <div class="row end_button">
+                <button class="waves-effect btn-flat" id="logout" token='<?php echo $_SESSION["token"] ?>'>Logout</button>
+            </div>
+        </div>
+        <div id="all_books">
+        <!--<div class="row">
+                <div class="col s3">
+                  <div class="card">
+                    <div class="card-image">
+                        <img class="materialboxed" data-caption="Frank Thilliez - Rever" src="https://images-na.ssl-images-amazon.com/images/I/51dLjnKP8XL.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                      <p>I am a very simple card. I am good at containing small bits of information.
+                          I am convenient because I require little markup to use effectively.</p>
+                      </div>
+                      <div class="card-action">
+                          <a href="#">This is a link</a>
+                      </div>
+                  </div>
+              </div>-->
+          </div>
+      </div>
+  </div>
+</body>
+</html>
