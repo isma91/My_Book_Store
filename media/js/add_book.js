5745dc69ff8400b7bca8393df8b348ae8f217ece
isma91 <ismaydogmus@gmail.com>
2016-06-07 14:45:12 +0200

    select and datapicker are displayed

--- media/js/add_book.js
+++ media/js/add_book.js
@@ -2,12 +2,17 @@
 /*jslint devel : true*/
 /*global $, document, this*/
 $(document).ready(function(){
-    var path_to_ajax = "public_api/index.php";
-    function press_enter (selector, go_function) {
-        $(document).on('keyup', selector, function(event) {
-            if (event.keyCode === 13) {
-                go_function();
-            }
-        });
-    }
+	var path_to_ajax = "public_api/index.php";
+	function press_enter (selector, go_function) {
+		$(document).on('keyup', selector, function(event) {
+			if (event.keyCode === 13) {
+				go_function();
+			}
+		});
+	}
+	$('.datepicker').pickadate({
+		selectMonths: true,
+		selectYears: 115
+	});
+	$('select').material_select();
 });
