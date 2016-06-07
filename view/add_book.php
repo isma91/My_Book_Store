d53962e09e84874829a6905fb01cb57eed7954d9
isma91 <ismaydogmus@gmail.com>
2016-06-07 16:35:12 +0200

    display the add bokk form

--- view/add_book.php
+++ view/add_book.php
@@ -12,17 +12,84 @@
         <div class="row mui-panel" id="the_menu">
             <h1 class="title">Welcome to My_Book_Store !!</h1>
             <h2 class="title">You can add a boook here !!</h2>
-            <div class="row end_button">
-                <button class="waves-effect btn-flat" id="logout" token='<?php echo $_SESSION["token"] ?>'>Logout</button>
-                <a class="waves-effect btn-flat" href="?page=add_book">Ajouter Livre</a>
-                <a class="waves-effect btn-flat" href="?page=add_customer">Ajouter Client</a>
-                <a class="waves-effect btn-flat" href="?page=add_command">Ajouter Commande</a>
-            </div>
+            <?php include "button.php"; ?>
         </div>
-        <div id="add_book">
-        add a book
-          </div>
-      </div>
-  </div>
+        <div id="add_book" class="row">
+            <form class="col s12" method="POST" action="public_api/index.php">
+                <input type="hidden" name="action" value="add_book">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="book_name" type="text" name="book_name">
+                        <label for="book_name">Name Of The Book</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="author" type="text" name="author">
+                        <label for="author">Author Of The Book</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="editor" type="text" name="editor">
+                        <label for="editor">Editor Of The Book</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <select name="type">
+                            <option value="apologue">Apologue</option>
+                            <option value="comic">Comic</option>
+                            <option value="epistolary">Epistolary</option>
+                            <option value="manga">Manga</option>
+                            <option value="novel" selected="true">Novel</option>
+                            <option value="poem">Poem</option>
+                            <option value="theater">Theater</option>
+                        </select>
+                        <label>Type Of Book</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <select multiple="true" name="kind" id="select_kind">
+                        <option value="action">Action</option>
+                        <option value="adventure">Adventure</option>
+                        <option value="detective">Detective</option>
+                        <option value="drama">Drama</option>
+                        <option value="erotic">Erotic</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="horror">Horror</option>
+                        <option value="humour">Humour</option>
+                        <option value="legend">Legend</option>
+                        <option value="mystery">Mystery</option>
+                        <option value="mythology">Mythology</option>
+                        <option value="romance">Romance</option>
+                        <option value="science fiction">Science Fiction</option>
+                        <option value="western">Western</option>
+                        </select>
+                        <label for="kind" id="label_kind">Kind Of Novel</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="file-field input-field">
+                      <div class="btn">
+                        <span>File</span>
+                        <input type="file" accept="image/*" name="cover">
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path" type="text">
+                    </div>
+                </div>
+                <div class="row">
+                    <label for="date">Year of publication</label>
+                    <input type="date" name="date" id="date" class="datepicker">
+                </div>
+                <div class="row end_button">
+                    <button class="waves-effect btn-flat">Add The Book</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
