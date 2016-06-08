e43512ae9b016c5866cd3875ca2fff9dfa28d2d0
isma91 <ismaydogmus@gmail.com>
2016-06-08 19:11:00 +0200

    adding and displaying better button

--- view/button.php
+++ view/button.php
@@ -1,6 +1,13 @@
 <div class="row end_button">
-	<button class="waves-effect btn-flat" id="logout" token='<?php echo $_SESSION["token"] ?>'>Logout</button>
 	<a class="waves-effect btn-flat" href="?page=add_book">Add Book</a>
 	<a class="waves-effect btn-flat" href="?page=add_customer">Add Customer</a>
 	<a class="waves-effect btn-flat" href="?page=add_command">Add Command</a>
+</div>
+<div class="row end_button">
+	<a class="waves-effect btn-flat" href="?page=books">All Books</a>
+	<a class="waves-effect btn-flat" href="?page=customers">All Customers</a>
+	<a class="waves-effect btn-flat" href="?page=commands">All Commands</a>
+</div>
+<div class="row end_button">
+	<button class="waves-effect btn-flat" id="logout" token='<?php echo $_SESSION["token"] ?>'>Logout</button>
 </div>
