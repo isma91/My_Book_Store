b35fd50a68ea7703a0f4743444db9c37961772b7
isma91 <ismaydogmus@gmail.com>
2016-06-08 19:57:59 +0200

    remove the comment

--- view/books.php
+++ view/books.php
@@ -1,38 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="description" content="My_Book_Store" />
-    <title>Welcome to My_Mini_Tweet</title>
-    <?php include "media.html" ?>
-    <script src="media/js/books.js"></script>
+  <meta charset="utf-8">
+  <meta name="description" content="My_Book_Store" />
+  <title>Welcome to My_Mini_Tweet</title>
+  <?php include "media.html" ?>
+  <script src="media/js/books.js"></script>
 </head>
 <body>
-    <div class="container" id="the_body">
-        <div class="row mui-panel" id="the_menu">
-            <h1 class="title">Welcome to My_Book_Store !!</h1>
-            <h2 class="title">Here is the list of all books !!</h2>
-            <?php include "button.php"; ?>
-        </div>
-        <div id="all_books">
-        <!--<div class="row">
-                <div class="col s3">
-                  <div class="card">
-                    <div class="card-image">
-                        <img class="materialboxed" data-caption="Frank Thilliez - Rever" src="https://images-na.ssl-images-amazon.com/images/I/51dLjnKP8XL.jpg">
-                        <span class="card-title">Card Title</span>
-                    </div>
-                    <div class="card-content">
-                      <p>I am a very simple card. I am good at containing small bits of information.
-                          I am convenient because I require little markup to use effectively.</p>
-                      </div>
-                      <div class="card-action">
-                          <a href="#">This is a link</a>
-                      </div>
-                  </div>
-              </div>-->
-          </div>
-      </div>
+  <div class="container" id="the_body">
+    <div class="row mui-panel" id="the_menu">
+      <h1 class="title">Welcome to My_Book_Store !!</h1>
+      <h2 class="title">Here is the list of all books !!</h2>
+      <?php include "button.php"; ?>
+    </div>
+    <div id="all_books">
+    </div>
   </div>
 </body>
 </html>
