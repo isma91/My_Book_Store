7098e0b45fbb5562c5791ede299ec44cf592c685
isma91 <ismaydogmus@gmail.com>
2016-06-09 22:56:30 +0200

    display all customers

---
+++ view/customers.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="My_Book_Store" />
+  <title>Welcome to My_Mini_Tweet</title>
+  <?php include "media.html" ?>
+  <script src="media/js/customers.js"></script>
+</head>
+<body>
+  <div class="container" id="the_body">
+    <div class="row mui-panel" id="the_menu">
+      <h1 class="title">Welcome to My_Book_Store !!</h1>
+      <h2 class="title">Here is the list of all customers !!</h2>
+      <?php include "button.php"; ?>
+    </div>
+    <div class="row" id="all_customers"></div>
+  </div>
+</body>
+</html>
