ab6c801309ffc2952941385f4c225808fce2a2f6
isma91 <ismaydogmus@gmail.com>
2016-06-09 23:00:25 +0200

    adding the form

--- view/add_customer.php
+++ view/add_customer.php
@@ -14,10 +14,42 @@
             <h2 class="title">You can add a customer here !!</h2>
             <?php include "button.php"; ?>
         </div>
+        <div id="add_customer_error"></div>
         <div id="add_customer">
-        add a customer
-          </div>
-      </div>
-  </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="first_name" type="text" name="first_name">
+                    <label for="first_name">Firstname</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="last_name" type="text" name="last_name">
+                    <label for="last_name">Lastname</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="adresse" type="text" name="adresse">
+                    <label for="adresse">Adresse</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="city" type="text" name="city">
+                    <label for="city">City</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="email" type="email" name="email">
+                    <label for="email">Email</label>
+                </div>
+            </div>
+            <div class="row end_button">
+                <button class="waves-effect btn-flat" id="validate_add_customer">Add The Customer</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
